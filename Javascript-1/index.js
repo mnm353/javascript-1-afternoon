a1b80304ea9cff6473c36c1bb67794f947812e04
mnm353 <mnm353@gmail.com>
2019-05-22 20:43:19 -0600

    solved all but a few problems

---
+++ Javascript-1/index.js
@@ -0,0 +1,5 @@
+var numbers = [4, 17, 80, 200, 37]
+
+var evens = numbers.filter(x => x % 2 === 0);
+  
+console.log(evens)
---
+++ objectDeclarations.js
@@ -0,0 +1,19 @@
+//Mandy
+const mandy = {
+    height: "5'6",
+    weight: '170',
+    eyeColor: 'Brown',
+    age: '31',
+    birthday: '12/03/1987',
+    email: 'mnm353@gmail.com',
+    isMale: false,
+    isFemale: true,
+    livesInUSA: true,
+    address: {
+        street: '746 W Alan Point Dr',
+        city: 'Draper',
+        state: 'Utah',
+        zipcode: '84020'
+    }
+}
+console.log(mandy.age)
